0b16194cfa622ed3babee952e63c47f5f7d67a3d
Duarte Fernandes <duarte_fq@hotmail.com>
2016-02-04 00:30:56 -0300

    agenda pronta

--- app/src/main/java/br/ifrn/tads/ddm/agendaaniversarios/MainActivity.java
+++ app/src/main/java/br/ifrn/tads/ddm/agendaaniversarios/MainActivity.java
@@ -1,13 +1,57 @@
 package br.ifrn.tads.ddm.agendaaniversarios;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.DatePickerDialog;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity implements DatePickerDialog.OnDateSetListener {
+
+    private TextView list;
+    private TextView previewDate;
+
+    private EditText nome;
+    private Button buttonInsert;
+
+    private Agenda agenda;
+    private String date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        agenda = new Agenda();
+        list = (TextView) findViewById(R.id.viewDate);
+        previewDate = (TextView) findViewById(R.id.previewDate);
+        nome = (EditText) findViewById(R.id.editTextNome);
+        buttonInsert = (Button) findViewById(R.id.buttonInsert);
+        date = "";
+
+        buttonInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                agenda.inserir(nome.getText().toString(), date);
+                list.setText(agenda.toString());
+            }
+        });
+
+
     }
+
+    public void onDateSet(DatePicker view, int year, int month, int day) {
+        date = day + "/" + (month + 1) + "/" + year;
+        previewDate.setText(date);
+    }
+
+    public void showDatePickerDialog(View v) {
+        DatePickerFragment newFragment = new DatePickerFragment();
+        newFragment.show(getFragmentManager(), "datePicker");
+    }
+
+
 }
